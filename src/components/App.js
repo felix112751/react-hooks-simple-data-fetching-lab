96cde02ef21248e5b9bf50826164deecc34bf3df
Felix Goyeh <felix.goyeh@student.moringaschool.com>
2024-08-13 08:22:20 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+// create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App() {
+    const [dogImage, setDogImage] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((res) => res.json())
+        .then((data) => {
+            setDogImage(data.message)
+            setIsLoading(false)
+        })
+        .catch((error) => {
+            //log any errors that occur during the fetch operation
+            console.error("Error fetching dog image:", error)
+            setIsLoading(false)
+        });
+    }, []);
+  return (
+    <div>
+        
+        {isLoading ?(
+            <p>Loading...</p>
+        ) : (
+            <img src={dogImage} alt='A Random Dog' style={{maxWidth: "100%"}} />
+        )}
+    </div>
+  )
+}
+
+export default App
